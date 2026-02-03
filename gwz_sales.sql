91bc8b33427636f1bf4c7da53d9b1117e0c5c8be
elierua1 <elieroukia@hotmail.fr>
2026-02-03 16:47:06 +0100

    update

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,8 +1,15 @@
 SELECT 
 date_date,
+<<<<<<< Updated upstream
 ROUND(SUM(turnover), 2) as round_daily_Turnover,
 ROUND(SUM(purchase_cost), 2) as round_purchase_cost
  FROM `euphoric-truth-456810-v6.course14.gwz_sales`
 
 GROUP BY date_date
 ORDER BY date_date DESC
+=======
+ROUND(SUM(turnover), 2) as daily_Turnover,
+ROUND(SUM(purchase_cost), 2) as purchase_cost
+ FROM `euphoric-truth-456810-v6.course14.gwz_sales`
+GROUP BY date_date
+>>>>>>> Stashed changes
